49a19e85504d903f49a13498466223612dab53d9
Dmitriy Grishkin <dima@grishkin.net>
2024-06-28 08:18:36 +0700

    initial project

---
+++ chips/chips/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chips
+//
+//  Created by Дмитрий Гришкин on 28.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
